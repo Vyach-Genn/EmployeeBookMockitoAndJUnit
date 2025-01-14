49a0973426906241d6c5fe45e80ba49941d86a49
vachg <vachgen@yandex.ru>
2025-01-14 21:36:23 +0300

    EmployeeBookMockitoAndJUnit

---
+++ src/main/java/mockitoandjunitemployeesbook/MockitoAndJUnitEmployeesBookApplication.java
@@ -0,0 +1,13 @@
+package mockitoandjunitemployeesbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockitoAndJUnitEmployeesBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MockitoAndJUnitEmployeesBookApplication.class, args);
+	}
+
+}
